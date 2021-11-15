2740da1554e00cc111d46f92de605d94114c2455
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-11-15 15:17:57 +0000

    Changed random string and container block to include count + output block using indexes

--- more-than-certified/terraform-docker/main.tf
+++ more-than-certified/terraform-docker/main.tf
@@ -19,12 +19,7 @@ resource "docker_image" "nodered_image" {
 
 # Define random string resources for names of containers
 resource "random_string" "random" {
-  length = 4
-  special = false
-  upper = false
-}
-
-resource "random_string" "random2" {
+  count = 2
   length = 4
   special = false
   upper = false
@@ -32,8 +27,9 @@ resource "random_string" "random2" {
 
 # Define docker container resource
 resource "docker_container" "nodered_container" {
+  count = 2 # 2 containers
   # Give it logical name if we need to reference it later - Use random string
-  name = join("-", ["nodered", random_string.random.result])
+  name = join("-", ["nodered", random_string.random[count.index].result])
   # Specify docker image and ref image we made above
   # .latest gives us ID of the image
   image = docker_image.nodered_image.latest
@@ -44,37 +40,25 @@ resource "docker_container" "nodered_container" {
   }
 }
 
-resource "docker_container" "nodered_container2" {
-  # Give it logical name if we need to reference it later
-  name = join("-", ["nodered", random_string.random2.result])
-  # Specify docker image and ref image we made above
-  # .latest gives us ID of the image
-  image = docker_image.nodered_image.latest
-  # Ports to expose on container + mapping
-  ports {
-    internal = 1880
-    #external = 1880
-  }
-}
 
 # Add output values referencing attribute of above container
 # Use join function to output ipaddr:port
 output "Ip_address_port" {
-  value       = join(":", [docker_container.nodered_container.ip_address, docker_container.nodered_container.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[0].ip_address, docker_container.nodered_container[0].ports[0].external])
   description = "IP addr & port of container"
 }
 
 output "container_name" {
-  value       = docker_container.nodered_container.name
+  value       = docker_container.nodered_container[0].name
   description = "Name of container"
 }
 
 output "Ip_address_port2" {
-  value       = join(":", [docker_container.nodered_container2.ip_address, docker_container.nodered_container2.ports[0].external])
+  value       = join(":", [docker_container.nodered_container[1].ip_address, docker_container.nodered_container[1].ports[0].external])
   description = "IP addr & port of container"
 }
 
 output "container_name2" {
-  value       = docker_container.nodered_container2.name
+  value       = docker_container.nodered_container[1].name
   description = "Name of container"
 }
