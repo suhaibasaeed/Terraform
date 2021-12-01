c962dc57af0b8337bcdd2b437765eb01190d1906
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-12-01 23:22:34 +0000

    Added 2nd module for influxdb image

--- more-than-certified/terraform-docker/main.tf
+++ more-than-certified/terraform-docker/main.tf
@@ -1,9 +1,16 @@
 
 # reference docker image resource from image module
-module "image" {
+module "nodered_image" {
   source = "./image"
   # Use key of image map to get image
-  image_in = var.image[terraform.workspace]
+  image_in = var.image["nodered"][terraform.workspace]
+}
+
+# 2nd image for DB
+module "influxdb_image" {
+  source = "./image"
+  # Use key of image map to get image
+  image_in = var.image["influxdb"][terraform.workspace]
 }
 
 module "container" {
@@ -13,13 +20,12 @@ module "container" {
   # Pass into module
   name_in = join("-", ["nodered", terraform.workspace, random_string.random[count.index].result])
   # Reference output from image module - We don't want module to module flow
-  image_in = module.image.image_out
+  image_in = module.nodered_image.image_out
   # Ports - Inside module block as this info defined in root
   int_port_in = var.int_port
   ext_port_in = var.ext_port[terraform.workspace][count.index]
   # Volume
   container_path_in = "/data"
-  host_path_in = "${path.cwd}/noderedvol"
   
 }
 # Define random string resources for names of containers
--- more-than-certified/terraform-docker/variables.tf
+++ more-than-certified/terraform-docker/variables.tf
@@ -3,9 +3,15 @@ variable "image" {
   type = map
   description = "Image for container"
   default = {
+  nodered = {
     dev = "nodered/node-red:latest"
     prod = "nodered/node-red:latest-minimal"
     }
+  influxdb = {
+    dev = "quay.io/influxdb/influxdb:v2.0.2"
+    prod = "quay.io/influxdb/influxdb:v2.0.2"
+  }
+}
 }
 # Add variables
 variable "ext_port" {
