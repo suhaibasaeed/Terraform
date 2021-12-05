e11986f009f67c7df70caffdaaa22b59a5263a77
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-12-05 12:13:44 +0000

    Added main.tf for networking module

--- more-than-certified/terraform-aws/networking/main.tf
+++ more-than-certified/terraform-aws/networking/main.tf
@@ -2,11 +2,18 @@
 
 # Will be used for VPC
 resource "random_integer" "random" {
-    min =  1
-    max = 100
+  min = 1
+  max = 100
 }
 
 resource "aws_vpc" "mtc_vpc" {
-    # Don't hardcode range as it's a module
-    cidr_block = 
+  # Don't hardcode range as it's a module
+  cidr_block = var.vpc_cidr
+  # So that we can have DNS hostnames for resources in VPC
+  enable_dns_hostnames = true
+  enable_dns_support_  = true
+
+  tags = {
+    Name = "mtc_vpc-${random_integer.random.id}"
+  }
 }
---
+++ more-than-certified/terraform-aws/networking/variables.tf
@@ -0,0 +1,5 @@
+# Networking/variables.tf
+
+variable "vpc_cidr" {
+  type = string
+}
