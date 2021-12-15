308f88dcf8d082cb93562df32fa0c8543724f7e9
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-12-15 14:07:24 +0000

    Added lifecycle block to target group resource

--- more-than-certified/terraform-aws/loadbalancing/main.tf
+++ more-than-certified/terraform-aws/loadbalancing/main.tf
@@ -14,6 +14,12 @@ resource "aws_lb_target_group" "mtc_tg" {
   port     = var.tg_port
   protocol = var.tg_protocol
   vpc_id   = var.vpc_id
+  lifecycle {
+      # Ignore any changes to resource name
+      ignore_changes = [name]
+      # Ensure new target group created before old is destroyed
+      create_before_destroy = true
+  }
   health_check {
     healthy_threshold   = var.lb_healthy_threshold
     unhealthy_threshold = var.lb_unhealthy_threshold
