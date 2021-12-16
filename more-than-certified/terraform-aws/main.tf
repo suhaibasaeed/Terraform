ae25163d4354b6b282e249318e415bdd92fc1865
Suhaib Saeed <saeed.suhaib@googlemail.com>
2021-12-16 14:32:18 +0000

    Added extra rule to SG for NGINX and increased instance count

--- more-than-certified/terraform-aws/main.tf
+++ more-than-certified/terraform-aws/main.tf
@@ -52,7 +52,7 @@ module "loadbalancing" {
 
 module "compute" {
   source         = "./compute"
-  instance_count = 1
+  instance_count = 2
   instance_type  = "t3.micro"
   public_sg      = module.networking.public_sg
   public_subnets = module.networking.public_subnets
